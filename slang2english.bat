6d7b126c5368ea29ba60f5d6ea07b74ea5cfe4dd
anic17 <58483910+anic17@users.noreply.github.com>
2021-07-26 17:20:58 +0200

    Added "rn" (= right now)

--- slang2english.bat
+++ slang2english.bat
@@ -7,7 +7,7 @@ if "%~1" neq "" (
 
 set /a c_sl=0, c_fe=0
 
-for %%A in (imo tbh idc idk idfk ig ik ikr lol lmfao lmao yh y ye yo tf wtf k cya tho ur r u w8 m8 l8 r8 gn gf bf smth tmrw smh p2w skid yall kys alr aight c wbu btw im youre atm 's boi jk fck fcking fu ftw) do (
+for %%A in (imo tbh idc idk idfk ig ik ikr lol lmfao lmao yh y ye yo tf wtf k cya tho ur r u w8 m8 l8 r8 gn gf bf smth tmrw smh p2w skid yall kys alr aight c wbu btw im youre atm 's boi jk fck fcking fu ftw rn) do (
     set "slang[!c_sl!]=%%A"
     set /a c_sl+=1
 )
@@ -18,7 +18,7 @@ for %%A in (
     "the fuck" "what the fuck" "ok" "see ya" "though" "your" "are" "you" "wait" "mate" "late" "rate"
     "good night" "girlfriend" "boyfriend" "something" "tomorrow" "shake my head" "pay to win" "script kiddie"
     "you all" "kill yourself" "alright" "alright" "see" "what about you" "by the way" "I'm" "you're" "at the moment" "is" "boy" "just kidding"
-    "fuck" "fucking" "fuck you" "for the win"
+    "fuck" "fucking" "fuck you" "for the win" "right now"
     ) do (
     set "fe[!c_fe!]=%%~A"
     set /a c_fe+=1
